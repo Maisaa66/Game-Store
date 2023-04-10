249c662e25ae0b65520aaeaa49444ca6e4b4a6e5
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-10 04:56:21 +0200

    Add functionality to get random set of games to display them as recommended

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,1 +1,4 @@
-export const getGame =(state)=>state.games[Math.floor(Math.random() * state.games.length)]
+export const getGame =(state)=>state.games[Math.floor(Math.random() * state.games.length)];
+
+//we will sort the array in random order and get numbe rof items
+export const getRecommendedGames =(state)=>state.games.sort(()=>0.5 - Math.random()).slice(0, 13);
