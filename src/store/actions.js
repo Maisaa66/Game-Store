e172ed496c90ebb7016d3866f01b58a4f810fba5
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-10 16:32:33 +0200

    Add Vuex getter to retrieve game genres
    This commit adds a new getter called `getGenres` to the Vuex store, which retrieves all unique game genres from the games api

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const getAllGames = ({commit})=>{
     axios.get("http://localhost:3000/Games")
-        .then((res)=> commit('SET_GAMES', res.data));
+        .then((res)=> commit('SET_GAMES', res.data))
+        
 }
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,15 @@
 export const getGame =(state)=>state.games[Math.floor(Math.random() * state.games.length)];
 
 //we will sort the array in random order and get numbe rof items
-export const getRecommendedGames =(state)=>state.games.sort(()=>0.5 - Math.random()).slice(0, 13);
+export const getRecommendedGames = (state)=>state.games.sort(()=>0.5 - Math.random()).slice(0, 13);
+
+export const getCategories = (state) => {
+    const genres = []
+    state.games.forEach((game) => {
+      if (!genres.includes(game.genre)) {
+        genres.push(game.genre)
+      }
+    })
+    return genres
+  }
+  
