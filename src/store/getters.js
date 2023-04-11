894ad3a316ef74da5cee5c9804bd640b262f3b9a
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-11 23:06:03 +0200

    add functionality to get popular games

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,18 @@
+let randomSorting = ()=>0.5 - Math.random();
 export const getGame =(state)=>state.games[Math.floor(Math.random() * state.games.length)];
 
 //we will sort the array in random order and get numbe rof items
-export const getRecommendedGames = (state)=>state.games.sort(()=>0.5 - Math.random()).slice(0, 13);
+export const getRecommendedGames = (state)=>{
+  let gamesCopy = [...state.games];
+  gamesCopy.sort(randomSorting).slice(0, 13);
+  return gamesCopy;
+};
+
+export const getPopularGames = (state)=>{
+  let gamesCopy = [...state.games];
+  gamesCopy.sort(randomSorting).slice(0, 13);
+  return gamesCopy;
+};
 
 export const getCategories = (state) => {
     const genres = []
