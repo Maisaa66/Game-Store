f00e9d92d2eef040e09fe88ba0d3936c45df8fdf
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-11 23:07:39 +0200

    create home page
    to display recommended and popular gamed

---
+++ src/Pages/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<featuredGame></featuredGame>
+<recommendedGames></recommendedGames>
+<popularGames></popularGames>
+</template>
+
+<script>
+import recommendedGames from '../components/recommended.vue';
+import popularGames from '../components/popular.vue';
+import featuredGame from '../components/featuredGame.vue';
+    export default {
+        name:"homePage",
+        components:{
+        recommendedGames,
+        popularGames,
+        featuredGame
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
